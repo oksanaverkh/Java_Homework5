e19389e0ed1f6fcac6b41c818a81ef6dab702e3d
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-02-23 21:24:11 +0300

    Task1 completed

--- Task1.java
+++ Task1.java
@@ -1,6 +1,24 @@
-// 1) Подсчитать количество вхождения каждого слова
-// Пример:
-// Россия идет вперед всей планеты. Планета — это не Россия.
-// toLoverCase().
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) {
+        Map<String, Integer> mapWord= new HashMap<>();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter a string: ");
+        String str = sc.nextLine();
+        sc.close();
 
+        for (int i = 0; i < (str.split("\\s+")).length; i++) {
+            mapWord.putIfAbsent(str.split("\\s+")[i].toLowerCase(), 0);
+            mapWord.put(str.split("\\s+")[i].toLowerCase(), mapWord.get(str.split("\\s+")[i].toLowerCase())+1);
+        }
+
+        for (Map.Entry<String, Integer> entry: mapWord.entrySet()) {
+            System.out.println(entry.getKey()+" meets: "+ entry.getValue()+" times");
+        }
+    }
+    
+}
